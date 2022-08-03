be1210e0046f52a996f3f571d2a2899a4380e595
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-08-03 22:30:59 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
 import Subject from './components/Subject';
+import Control from './components/Control';
 import './App.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
+        this.max_content_id = 3;
         this.state = {
+            mode: "create",
+            selected_content_id: 2,
             subject: {
                 title: "WEB",
                 sub: "World Wide Web!",
+            },
+            welcome: {
+                title: "Welcome",
+                desc: "Hello React!!",
             },
             contents: [
                 {id: 1, title: "HTML", desc: "HTML is for information"},
@@ -20,14 +29,73 @@ class App extends Component {
         }
     }
     render() {
+        let _title, _desc, _article = null;
+
+        if (this.state.mode === "welcome") {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+            _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+        } else if (this.state.mode === "read") {
+            let i = 0;
+
+            while(i < this.state.contents.length) {
+                let data = this.state.contents[i];
+
+                if (data.id === this.state.selected_content_id) {
+                    _title = data.title;
+                    _desc = data.desc;
+
+                    break;
+                }
+
+                i += 1;
+            }
+
+            _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+        } else if (this.state.mode === "create") {
+            _article = <CreateContent onSubmit={function(_title, _desc){
+                // add content to this.state.contents
+                this.max_content_id += 1;
+
+                let _contents = this.state.contents.concat({
+                    id: this.max_content_id,
+                    title: _title,
+                    desc: _desc,
+                });
+
+                this.setState({
+                    contents: _contents,
+                });
+            }.bind(this)}></CreateContent>;
+        }
+
         return (
-            <div className="App">
+            <div className='App'>
                 <Subject 
                     title={this.state.subject.title} 
-                    sub={this.state.subject.sub}>
+                    sub={this.state.subject.sub}
+                    onChangePage={function() {
+                        this.setState({
+                            mode: "welcome",
+                        });
+                    }.bind(this)}
+                >
                 </Subject>
-                <TOC data={this.state.contents}></TOC>
-                <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+                <TOC 
+                    data={this.state.contents}
+                    onChangePage={function(id) {
+                        this.setState({
+                            mode: "read",
+                            selected_content_id: Number(id),
+                        });
+                    }.bind(this)}
+                ></TOC>
+                <Control onChangeMode={function(_mode){
+                    this.setState({
+                        mode: _mode,
+                    });
+                }.bind(this)}></Control>
+                {_article}
             </div>
         );
     }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -6,8 +6,20 @@ class TOC extends Component {
         let data = this.props.data;
         let i = 0;
 
-        while (i < data.length) {
-            lists.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>)
+        while(i < data.length) {
+            lists.push(
+                <li key={data[i].id}>
+                    <a 
+                        href={"/content/" + data[i].id}
+                        onClick={function(id, e) {
+                            e.preventDefault();
+
+                            this.props.onChangePage(id);
+                        }.bind(this, data[i].id)}
+                    >{data[i].title}</a>
+                </li>
+            );
+
             i += 1;
         }
 
