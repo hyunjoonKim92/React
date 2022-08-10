063015b1ef508fe1b863970bbdeb7b82cb3ed7ac
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-08-10 17:06:03 +0900

    -- 220810 커밋 --- delete 구현

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         super(props);
         this.max_content_id = 3;
         this.state = {
-            mode: "create",
+            mode: "welcome",
             selected_content_id: 2,
             subject: {
                 title: "WEB",
@@ -125,9 +125,33 @@ class App extends Component {
                     }.bind(this)}
                 ></TOC>
                 <Control onChangeMode={function(_mode) {
-                    this.setState({
-                        mode: _mode,
-                    });
+                    if (_mode === "delete") {
+                        if (window.confirm("really?")) {
+                            let _contents = Array.from(this.state.contents);
+                            let i = 0;
+
+                            while(i < _contents.length) {
+                                if (_contents[i].id === this.state.selected_content_id) {
+                                    _contents.splice(i, 1);
+
+                                    break;
+                                }
+
+                                i += 1;
+                            }
+
+                            this.setState({
+                                mode: "welcome",
+                                contents: _contents,
+                            });
+
+                            alert("deleted!");
+                        }
+                    } else {
+                        this.setState({
+                            mode: _mode,
+                        });
+                    }
                 }.bind(this)}></Control>
                 {this.getContent()}
             </div>
